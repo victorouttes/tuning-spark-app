f7d22fc8d49f0abfa952072474d1f837e9fcb5ca
Victor <victor.outtes@gmail.com>
2024-07-08 18:10:46 -0300

    ajustes

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -18,7 +18,7 @@ def init_spark_session(app_name):
     builder = (
         pyspark.sql.SparkSession.builder.appName(app_name)
         .config("spark.sql.adaptive.enabled", "true")
-        .config("spark.executor.memory", "3g")
+        .config("spark.executor.memory", "1g")
         .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
         .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
         .config("spark.hadoop.fs.s3a.access.key", "minio") 
